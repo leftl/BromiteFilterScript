0f678aefea04e636179ffb4d38c3f8d71fa412b1
Tyler Waters <19383474+leftl@users.noreply.github.com>
2022-02-13 11:48:58 -0800

    resolve ruleset_converter location change

--- makefilters.sh
+++ makefilters.sh
@@ -3,8 +3,7 @@
 # Download the ruleset converter if we dont have it already
 BIN="ruleset_converter"
 if [[ ! -x ${BIN} ]]; then
-    URL="$(curl -sLI -o /dev/null -w '%{url_effective}' 'https://github.com/bromite/filters/releases/latest/')"
-    URL="${URL//tag/download}/${BIN}"
+    URL="https://github.com/bromite/bromite/releases/downloads/latest/${BIN}"
     echo "Downloading ${URL}"
     curl -L "${URL}" -o ${BIN}
     if file ${BIN}|grep -q "ELF 64-bit LSB"; then
